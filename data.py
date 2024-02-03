69339bdb0bac7b3f8a232450b575690a0e549bf2
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2024-02-03 12:55:42 -0500

    Added controller class

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 class Data:
-    self.bicep_flex
-    self.lean_forward
+    bicep_flex: int
+    lean_forward: int
 
-    def __init__(bicep_flex, lean_forward):
+    def __init__(self, bicep_flex, lean_forward):
         self.bicep_flex = bicep_flex
         self.lean_forward = lean_forward
