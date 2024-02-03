102a402b4cc9d18bf723871f8c7f61ae6b1ec820
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2024-02-03 15:19:20 -0500

    rest of stuff

--- data.py
+++ data.py
@@ -1,15 +1,23 @@
 class GameActionData:
     flex_value: int
     jump_value: int
+    forward_value: int
+    backward_value: int
+    left_value: int
+    right_value: int
+    tiltup_value: int
+    tiltdown_value: int
+    tiltleft_value: int
+    tiltright_value: int
 
-    def __init__(self, flex_value, jump_value):
+    def __init__(self, flex_value, jump_value, forward_value, backward_value, left_value, right_value, tiltup_value, tiltdown_value, tiltleft_value, tiltright_value):
         self.flex_value = flex_value
         self.jump_value = jump_value
-
-class RotationData:
-    x_value: int
-    y_value: int
-
-    def __init__(self, y_value, x_value):
-        self.x_value = y_value
-        self.y_value = x_value
+        self.forward_value = forward_value
+        self.backward_value = backward_value
+        self.left_value = left_value
+        self.right_value = right_value
+        self.tiltup_value = tiltup_value
+        self.tiltdown_value = tiltdown_value
+        self.tiltleft_value = tiltleft_value
+        self.tiltright_value = tiltright_value
--- minecraft_controller.py
+++ minecraft_controller.py
@@ -7,11 +7,19 @@ from data import GameActionData, RotationData
 
 class MinecraftController:
     flex_threshold = 150
-    jump_threshold = 90
+    jump_threshold = 3000
+    forward_threshold = 3000
+    backward_threshold = -3000
+    left_threshold = -5000
+    right_threshold = -5000
+    uptilt_threshold = -15000
+    downtilt_threshold = 15000    
+    lefttilt_threshold = -15000
+    righttilt_threshold = 15000
 
     pressing = False
+    walking = [False, False, False, False]
     keyboard = Controller()
-    
 
     def start_hit(self):
         print("started hitting")
@@ -30,15 +38,51 @@ class MinecraftController:
     def select_axe():
         pyautogui.press('3')
 
-    def game_actions(self, data: GameActionData):
-        if (data.flex_value >= self.flex_threshold and not self.pressing):
+    def check_punch_start(self, flex_value):
+        if (flex_value >= self.flex_threshold and not self.pressing):
             Thread(target=self.start_hit).start()
             self.pressing = True
-        if (data.flex_value < self.flex_threshold and self.pressing):
+        if (flex_value < self.flex_threshold and self.pressing):
             Thread(target=self.end_hit).start()
             self.pressing = False
-        if (data.jump_value >= self.jump_threshold):
+
+    def check_start_walk(self, forward, backward, left, right):
+        if (forward >= self.forward_threshold and self.walking[0]):
+            Thread(self.keyboard.press('w')).start()
+            self.walking[0] = True
+        if (backward <= self.backward_threshold and self.walking[1]):
+            Thread(self.keyboard.press('s')).start()
+            self.walking[1] = True        
+        if (right >= self.right_threshold and self.walking[2]):
+            Thread(self.keyboard.press('d')).start()
+            self.walking[2] = True        
+        if (left <= self.left_threshold and self.walking[3]):
+            Thread(self.keyboard.press('a')).start()
+            self.walking[3] = True
+
+    def check_end_walk(self, forward, backward, left, right):
+        if (forward < self.forward_threshold and not self.walking[0]):
+            Thread(self.keyboard.release('w')).start()
+            self.walking[0] = False
+        if (backward > self.backward_threshold and not self.walking[1]):
+            Thread(self.keyboard.release('s')).start()
+            self.walking[1] = False        
+        if (right < self.right_threshold and not self.walking[2]):
+            Thread(self.keyboard.release('d')).start()
+            self.walking[2] = False        
+        if (left > self.left_threshold and not self.walking[3]):
+            Thread(self.keyboard.release('a')).start()
+            self.walking[3] = False
+
+    def check_jump(self, jump_value):
+        if (jump_value >= self.jump_threshold):
             Thread(target=self.jump).start()
+
+    def game_actions(self, data: GameActionData):
+        self.check_punch_start(data.flex_value)
+        self.check_start_walk(data.forward_value, data.backward_value, data.left_value, data.right_value)
+        self.check_end_walk(data.forward_value, data.backward_value, data.left_value, data.right_value)
+        self.rotate_camera(data.tiltup_value, data.tiltdown_value, data.tiltleft_value, data.tiltright_value)
 
     def select_item(self, item: int):
         if (item == 2):
@@ -46,9 +90,13 @@ class MinecraftController:
         elif (item == 3):
             self.select_axe()
 
-    def rotate_camera(self, data: RotationData):
-        y_value = data.x_value / 10
-        x_value = data.y_value / 10
-        pyautogui.moveRel(200 * x_value, 300 * y_value, duration=1)
-
+    def rotate_camera(self, tiltup_value, tiltdown_value, tiltleft_value, tiltright_value):
+        if (tiltup_value <= self.uptilt_threshold):
+            Thread(pyautogui.moveRel(0, 30, duration=1)).start()
+        if (tiltdown_value >= self.downtilt_threshold):
+            Thread(pyautogui.moveRel(0, -30, duration=1)).start()
+        if (tiltleft_value <= self.lefttilt_threshold):
+            Thread(pyautogui.moveRel(20, 0, duration=1)).start()
+        if (tiltright_value >= self.righttilt_threshold):
+            Thread(pyautogui.moveRel(-20, 0, duration=1)).start()
 
