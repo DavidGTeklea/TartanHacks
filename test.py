cbb592af81a34a1ddedf28d1a74e88bd44e9f026
DavidGTeklea <87445117+DavidGTeklea@users.noreply.github.com>
2024-02-03 13:10:11 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from data import Data
 from minecraft_controller import MinecraftController
+import time 
 
-
+time.sleep(3)
 dig_test = [0, 0, 0, 0, 0, 0, 100, 100, 100, 100, 0, 0, 0, 0]
 hit_test = [0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100]
 
 mc_controller = MinecraftController()
 
-for num in dig_test:
+for num in hit_test:
     data = Data(bicep_flex=num, lean_forward=0)
+    time.sleep(0.5)
     mc_controller.game_actions(data)
