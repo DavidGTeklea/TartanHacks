a0540d5e81e1af8c18678be710781e75a3d8fdcf
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2024-02-03 14:38:49 -0500

    Add classifier call

--- DataMedium.py
+++ DataMedium.py
@@ -1,6 +1,8 @@
 import serial
 import time
 import struct
+from Classifier.classifier import take_screenshot, weapon_classifier
+from roboflow import Roboflow
 
 # data connection to arduino
 ser = serial.Serial('/dev/cu.usbmodem101', 9600, timeout=1)
@@ -13,6 +15,13 @@ Accelerometer_Value_Default_Head = [-2000.0, -1000.0, 18000.0]
 Gyroscope_Value_Default_Head = [-6000.0, -9000.0, 11500.0]
 Accelerometer_Value_Default_Chest = [-1100.0, 18000.0, -1000.0]
 Gyroscope_Value_Default_Chest= [100.0, -9000.0, 11500.0]
+
+rf = Roboflow(api_key="c5Ak0Ld4PWZ1PNR0nt23")
+mob_project = rf.workspace().project("minecraft-mob-detection")
+mob_model = mob_project.version(10).model
+
+tree_project = rf.workspace().project("minecraft-tree-detection")
+tree_model = tree_project.version(1).model
 
 try:
     while True:
@@ -28,13 +37,16 @@ try:
             Accelerometer_Value_Chest = DataPackage[7:10]
             Gyroscope_Value_Chest = DataPackage[10:]
 
-            print("----------------")
-            print("EMG Value: " + str(EMG_Value_Bicep))
-            print("Accel Head Value: " + str(Accelerometer_Value_Head[0]) + ", " + str(Accelerometer_Value_Head[1]) + ", " + str(Accelerometer_Value_Head[2]))
+            # print("----------------")
+            # print("EMG Value: " + str(EMG_Value_Bicep))
+            # print("Accel Head Value: " + str(Accelerometer_Value_Head[0]) + ", " + str(Accelerometer_Value_Head[1]) + ", " + str(Accelerometer_Value_Head[2]))
             # print("Gyro Head Value: " + str(Gyroscope_Value_Head[0]) + ", " + str(Gyroscope_Value_Head[1]) + ", " + str(Gyroscope_Value_Head[2]))
             # print("Accel Chest Value: " + str(Accelerometer_Value_Chest[0]) + ", " + str(Accelerometer_Value_Chest[1]) + ", " + str(Accelerometer_Value_Chest[2]))
             # print("Gyro Chest Value: " + str(Gyroscope_Value_Chest[0]) + ", " + str(Gyroscope_Value_Chest[1]) + ", " + str(Gyroscope_Value_Chest[2]))
             
+            item = weapon_classifier(take_screenshot())
+            
+
 except KeyboardInterrupt:
     ser.close()
     print("Serial connection closed.")
